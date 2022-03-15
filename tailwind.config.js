baac275dc010dc9b9a3b8ea49d6d158b2be7f24a
Dilate - Jejo <developers@dilate.com.au>
2022-03-15 17:20:56 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './assets/*.liquid',
+    './templates/*.liquid',
+    './templates/**/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
